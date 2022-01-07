c024f7e057390d3b8a099a4e8606136a739287fe
João Santos <joao.santos.developer@gmail.com>
2022-01-07 19:22:32 +0000

    Setup project structure

---
+++ app/src/main/java/com/joaodevsantos/noteapp/feature_note/presentation/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.joaodevsantos.noteapp.feature_note.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            // TODO
+        }
+    }
+}
